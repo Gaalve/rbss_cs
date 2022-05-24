388e8ac65eb42a4a6bf5a1f7b14f65d0d221e849
algorithmv3 <adrian.schwaiger@campus.tu-berlin.de>
2022-05-24 15:33:58 +0200

    p2p models and api stubs added

---
+++ RBBS_CS/Controllers/PeerNetworkApi.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.RBB_CS;
+using Org.OpenAPIToolsServer.Controllers;
+
+namespace RBBS_CS.Controllers
+{
+    public class PeerNetworkApi : PeerNetworkApiController
+    {
+        public override IActionResult ExitPost(Successor successor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IActionResult JoinPost(Joining joining)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IActionResult NotifyPost(Predecessor predecessor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
