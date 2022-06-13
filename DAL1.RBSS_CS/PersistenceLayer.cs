ab7d55d9891160524903400b78ff4aa98d8aa62a
algorithmv3 <adrian.schwaiger@campus.tu-berlin.de>
2022-06-13 14:36:52 +0200

    fixed bugs regarding empty sets and sets containing one element

--- DAL1.RBSS_CS/PersistenceLayer.cs
+++ DAL1.RBSS_CS/PersistenceLayer.cs
@@ -1,5 +1,4 @@
-﻿using DAL1.RBB_CS;
-using Models.RBB_CS;
+﻿using Models.RBSS_CS;
 
 namespace DAL1.RBSS_CS
 {
@@ -87,10 +86,11 @@ namespace DAL1.RBSS_CS
 
         private RangeSet[] SplitRange(string id)
         {
+            RangeSet[] ranges = new RangeSet[2];
+            if (_set.Count == 0) return ranges;
             var lower = new SimpleObjectWrapper(id);
             var upper = _set.Last();
-            RangeSet[] ranges = new RangeSet[2];
-            var midId = _set.Select(s => s.Data.Id).ToArray()[(1 + _set.Count) / 2];
+            var midId = _set.Count == 1 ? upper.Data.Id : _set.Select(s => s.Data.Id).ToArray()[(1 + _set.Count) / 2];
             var mid = new SimpleObjectWrapper(midId);
             var range1 = _set.GetViewBetween(lower, mid);
             var range2 = _set.GetViewBetween(mid, upper);
@@ -102,12 +102,14 @@ namespace DAL1.RBSS_CS
 
         public RangeSet[] SplitRange(string idFrom, string idTo)
         {
+            if (_set.Count == 0) return new RangeSet[2];
             if (idFrom == idTo) return SplitRange(idFrom);
             var lower = new SimpleObjectWrapper(idFrom);
             var lastExceeded = string.Compare(idFrom, idTo, StringComparison.Ordinal) > 0;
             var upper = lastExceeded ? _set.Last() : new SimpleObjectWrapper(idTo);
             RangeSet[] ranges = new RangeSet[2];
             var subset = _set.GetViewBetween(lower, upper);
+            if (subset.Count == 0) return ranges;
             var offset = subset.Last().Data.Id.Equals(upper.Data.Id) && !lastExceeded? 0 : 1;
             var midId = subset.Count == 1 ? subset.First().Data.Id : subset.Select(s => s.Data.Id).ToArray()[(subset.Count + offset) / 2];
             var mid = new SimpleObjectWrapper(midId);
