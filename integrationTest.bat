c7569cbff2a72dc7306517396696a31ed2af853c
algorithmv3 <adrian.schwaiger@campus.tu-berlin.de>
2022-07-31 20:09:43 +0200

    added fixedSeed to random update tests

--- integrationTest.bat
+++ integrationTest.bat
@@ -1,4 +1,4 @@
-docker-compose build
+REM docker-compose build
 docker-compose up --abort-on-container-exit | grep initiator
 docker cp rbbs_cs_rbss_cs_initiator_1:/app/memResults.log memResults.log
 docker cp rbbs_cs_rbss_cs_initiator_1:/app/testResults.log testResults.log
